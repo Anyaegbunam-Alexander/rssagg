70de6f9282619d6ad8ec70433577e4df93a681f3
Alexander Anyaegbunam <108991664+Anyaegbunam-Alexander@users.noreply.github.com>
2023-12-16 15:55:27 +0000

    feeds

--- handler_user.go
+++ handler_user.go
@@ -6,17 +6,16 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/Anyaegbunam-Alexander/rssagg/internal/auth"
 	"github.com/Anyaegbunam-Alexander/rssagg/internal/database"
 	"github.com/google/uuid"
 )
 
-func (apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Name string `json:"name"`
 	}
 	decoder := json.NewDecoder(r.Body)
-	
+
 	params := parameters{}
 	err := decoder.Decode(&params)
 	if err != nil {
@@ -25,10 +24,10 @@ func (apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request
 	}
 
 	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
-		ID: uuid.New(),
+		ID:        uuid.New(),
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
-		Name: params.Name,
+		Name:      params.Name,
 	})
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user %v", err))
@@ -37,19 +36,7 @@ func (apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request
 
 	respondWithJSON(w, 201, databaseUserToUser(user))
 }
-func (apiCfg *apiConfig)handlerGetUser(w http.ResponseWriter, r *http.Request) {
-	apikey, err := auth.GetAPIKey(r.Header)
-	if err != nil {
-		respondWithError(w, 403, fmt.Sprintf("Auth error %v", err))
-		return
-	}
-	
-	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apikey)
-	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't get user %v", err))
-		return
-	}
 
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
 	respondWithJSON(w, 200, databaseUserToUser(user))
-	
 }
